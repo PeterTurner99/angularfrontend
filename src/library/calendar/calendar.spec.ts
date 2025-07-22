7ddccf2e23e22489530a47c88dc04e7faef55a33
PeterTurner99 <peterturner99p+github@gmail.com>
2025-07-22 16:08:19 +0100

    setting up test envi for vscode

--- src/library/calendar/calendar.spec.ts
+++ src/library/calendar/calendar.spec.ts
@@ -1,14 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  ComponentFixtureAutoDetect,
+  TestBed,
+} from '@angular/core/testing';
 
 import { Calendar } from './calendar';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 describe('Calendar', () => {
   let component: Calendar;
   let fixture: ComponentFixture<Calendar>;
-
+  let one_set_data = [
+    [
+      {
+        title: 'title test',
+        description: 'description test',
+        start: '123',
+        end: '1324',
+        id: 1,
+      },
+    ],
+  ];
+  let empty_data = [[]];
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Calendar],
@@ -21,11 +37,21 @@ describe('Calendar', () => {
 
     fixture = TestBed.createComponent(Calendar);
     component = fixture.componentInstance;
-    fixture.componentRef.setInput('data',[[]])
+    fixture.componentRef.setInput('data', empty_data);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should be empty without data', () => {
+    let elemSearch = fixture.debugElement.queryAll(By.css('.data-card'));
+    expect(elemSearch.length).toBeFalsy();
+  });
+//   it('should have one elemenmt when given one data element', () => {
+//     fixture.componentRef.setInput('data', one_set_data);
+//     fixture.detectChanges();
+//     let elemSearch = fixture.debugElement.queryAll(By.css('.data-card'));
+//     expect(elemSearch.length).toBe(1);
+//   });
 });
