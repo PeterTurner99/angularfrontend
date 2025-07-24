f2b2c4cfe9296d07ac94b3f04e235013d5c2d51e
PeterTurner99 <peterturner99p+github@gmail.com>
2025-07-24 15:12:30 +0100

    submitted card tests added

--- src/library/submitted-card/submitted-card.spec.ts
+++ src/library/submitted-card/submitted-card.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmittedCard } from './submitted-card';
+import { By } from '@angular/platform-browser';
 
 describe('SubmittedCard', () => {
   let component: SubmittedCard;
@@ -20,4 +21,16 @@ describe('SubmittedCard', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should be shown when passed true', ()=> {
+    fixture.componentRef.setInput('submitted', true );
+    fixture.detectChanges();
+    let elemSearch = fixture.debugElement.query(By.css('div'));
+    expect( elemSearch.nativeElement.hidden).toBeFalse()
+  })
+  it('should be hidden when passed false', ()=> {
+    fixture.componentRef.setInput('submitted', false );
+    fixture.detectChanges();
+    let elemSearch = fixture.debugElement.query(By.css('div'));
+    expect( elemSearch.nativeElement.hidden).toBeTrue()
+  })
 });
